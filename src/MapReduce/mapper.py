ca723f4603d8865b0f77161ff8f08495d2a1230f
FelipePM01 <fpachecomanoel@uol.com.br>
2022-11-11 15:57:30 -0300

    translated mapper and reducer to python and added some more functionalities

---
+++ src/MapReduce/mapper.py
@@ -0,0 +1,21 @@
+
+
+import sys
+
+for line in sys.stdin:
+    
+    line_format=line.split() # contains the index of the person and the list of friends
+    person=line_format[0]
+    if len(line_format)>1:
+        line_format[1]=line_format[1].split(',')#line_format1 is a array of friends now
+        for friend1 in line_format[1]:
+            for friend2 in line_format[1]:
+                if friend1!= friend2:
+                    print("%s\t%s\t%d"%(friend1,friend2,1))#here friend 1 is the key and friend 2 is value and the last number shows if the key value pair is 1 than they have at least 1 friend in common , if it is 0 then they are direct friends
+            print("%s\t%s\t%d"%(person,friend1,0))
+    else:
+        print("%s\t%s\t%d"%(person,person,0))
+    
+    
+
+    
---
+++ src/MapReduce/reducer.py
@@ -0,0 +1,32 @@
+
+import sys
+def output_recommendations(my_key,friends):
+    friends_list=friends.items()
+    friends_list=sorted(friends_list,key=lambda x:x[0])
+    friends_list=sorted(friends_list,key=lambda x:x[1],reverse=True)
+    top10=friends_list[0:10]
+    top10_formatted=[]
+    for i in top10:
+        if i[1]!=0:
+            top10_formatted.append(i[0])
+    output=",".join([str(x) for x in top10_formatted])
+    print("%d\t%s"%(my_key,output))
+        
+last_key=-1
+new_friends={}
+for line in sys.stdin:
+    line_format=line.split("\t")
+    key=int(line_format[0])
+    friend_id=int(line_format[1])
+    not_friend_yet=int(line_format[2])
+    if last_key!=key and last_key!=-1:
+        output_recommendations(last_key,new_friends)
+        new_friends={}
+    last_key=key
+    if friend_id not in new_friends:
+        new_friends[friend_id]=not_friend_yet
+    else:
+        new_friends[friend_id]=(new_friends[friend_id]+not_friend_yet) if (new_friends[friend_id] and not_friend_yet) else 0
+output_recommendations(last_key,new_friends)
+
+    
